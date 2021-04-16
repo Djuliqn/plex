371daf3f04e82af1f53521416a2ee84b43cae929
dzhuliyan.ovcharov <djuliqn.bg@gmail.com>
2021-04-16 09:32:33 +0300

    Add checkstyle.xml

--- src/main/java/com/plex/PlexApplication.java
+++ src/main/java/com/plex/PlexApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlexApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PlexApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlexApplication.class, args);
+    }
 
 }
