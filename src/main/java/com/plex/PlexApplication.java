04f1d655ac661987e7b6db55826930164eee31d4
dzhuliyan.ovcharov <djuliqn.bg@gmail.com>
2021-04-17 20:05:49 +0300

    add database connection and jasypt password encryption

--- src/main/java/com/plex/PlexApplication.java
+++ src/main/java/com/plex/PlexApplication.java
@@ -1,8 +1,10 @@
 package com.plex;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class PlexApplication {
     public static void main(String[] args) {
